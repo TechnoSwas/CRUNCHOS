3144ce9bc4321ae09ffd1ea64225673300e0e2ed
TechnoSwas <swas.swastik@hotmail.com>
2026-05-11 16:25:28 +0000

    save CRUNCHOS progress

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,13 +1,13 @@
-void kernel_main() {
-    volatile unsigned short* vga = (unsigned short*)0xB8000;
+#include <stdint.h>
 
-    vga[0] = (0x0A << 8) | 'K';
-    vga[1] = (0x0A << 8) | 'O';
-    vga[2] = (0x0A << 8) | 'S';
+void kernel_main(void) {
+    volatile uint16_t* vga = (uint16_t*)0xB8000;
+
+    vga[0] = 0x0F00 | 'C';
+    vga[1] = 0x0F00 | 'R';
+    vga[2] = 0x0F00 | 'U';
+    vga[3] = 0x0F00 | 'N';
+    vga[4] = 0x0F00 | 'C';
 
     while (1) {}
-}
-
-void _start() {
-    kernel_main();
 }
