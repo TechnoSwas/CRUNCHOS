e0e62350b834347caeb642db1ae58280cf6e16ff
TechnoSwas <swas.swastik@hotmail.com>
2026-05-13 04:21:50 +0000

    CrunchOS kernel boots and prints via serial!

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,13 +1,33 @@
 #include <stdint.h>
 
+#define MULTIBOOT_MAGIC 0x1BADB002
+#define MULTIBOOT_FLAGS 0x00
+#define MULTIBOOT_CHECKSUM -(MULTIBOOT_MAGIC + MULTIBOOT_FLAGS)
+
+__attribute__((section(".multiboot")))
+const unsigned int multiboot_header[] = {
+    MULTIBOOT_MAGIC,
+    MULTIBOOT_FLAGS,
+    MULTIBOOT_CHECKSUM
+};
+
+void serial_write(char c) {
+    __asm__ volatile ("outb %0, %1" : : "a"(c), "Nd"(0x3F8));
+}
+
+void serial_print(const char* msg) {
+    while (*msg) serial_write(*msg++);
+}
+
 void kernel_main(void) {
+    serial_print("Welcome to CrunchOS!\n");
+
     volatile uint16_t* vga = (uint16_t*)0xB8000;
-
-    vga[0] = 0x0F00 | 'C';
-    vga[1] = 0x0F00 | 'R';
-    vga[2] = 0x0F00 | 'U';
-    vga[3] = 0x0F00 | 'N';
-    vga[4] = 0x0F00 | 'C';
-
+    const char* msg = "Welcome to CrunchOS!";
+    int i = 0;
+    while (msg[i]) {
+        vga[i] = 0x1F00 | msg[i];
+        i++;
+    }
     while (1) {}
 }
